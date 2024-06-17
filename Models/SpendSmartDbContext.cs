8b2151e3d9ae2f383307b19f12a6869a5ef73bc7
parve <parve@PARVEEN>
2024-06-17 17:29:21 -0400

    Add project files.

---
+++ Models/SpendSmartDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SpendSmart.Models
+{
+    public class SpendSmartDbContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        public SpendSmartDbContext(DbContextOptions<SpendSmartDbContext> options)
+            : base(options)
+        {
+                
+        }
+    }
+}
